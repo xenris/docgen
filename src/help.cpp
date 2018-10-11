e637a549065f813e8c8240215a57e1599ab9b2c1
Henry Shepperd <hshepperd@gmail.com>
2018-10-11 22:28:36 +1100

    List default output directory in help

--- src/help.cpp
+++ src/help.cpp
@@ -4,7 +4,7 @@ const char* helpText =
     "-c [comment style] -> sets the style of comment which leads documentation, e.g. \"///\" (default), \"###\", etc...\n"
     "-h -> Show this help text.\n"
     "-d -> Delete docs directory before generating new documents.\n"
-    "-o [output directory] -> Set directory to put documents in.\n"
+    "-o [output directory] -> Set directory to put documents in. Default is \"docs/\".\n"
     "[file list] -> File paths to include.\n"
     "Example: docgen -c \"###\" main.hpp other.hpp\n"
     "Wildcard example: docgen $(find src/ -name '*.hpp')\n";
